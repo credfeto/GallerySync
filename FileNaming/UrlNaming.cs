2d9f5c7c450291fd2269bfe5a8244c4f13bcfbe0
Mark Ridgwell <github@markridgwell.com>
2014-04-29 23:02:45 +0100

    Removed hyphens around a /

--- FileNaming/UrlNaming.cs
+++ FileNaming/UrlNaming.cs
@@ -14,15 +14,20 @@ namespace FileNaming
                                                                      RegexOptions.Compiled |
                                                                      RegexOptions.CultureInvariant);
 
+        private static readonly Regex NoHyphensNextToSlash = new Regex(@"(\-*/\-*)",
+                                                                       RegexOptions.Compiled |
+                                                                       RegexOptions.CultureInvariant);
+
         public static string BuildUrlSafePath(string basePath)
         {
             string root = basePath.Trim() + "/";
 
-            return
+
+            return NoHyphensNextToSlash.Replace(
                 NoRepeatingHyphens.Replace(
                     AcceptableUrlCharacters.Replace(root.Replace(@"\", @"/"), Replacementchar),
-                    Replacementchar)
-                                  .TrimEnd(Replacementchar.ToCharArray()).ToLowerInvariant();
+                    Replacementchar),
+                "/").TrimEnd(Replacementchar.ToCharArray()).ToLowerInvariant();
         }
     }
 }
