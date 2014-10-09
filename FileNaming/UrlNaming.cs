93e3fb2eb1421b2f9d8456d4758f166b1e0764c6
Mark Ridgwell <github@markridgwell.com>
2014-10-10 00:37:30 +0100

    Changed Accented Characters into ones without accents for urls

--- FileNaming/UrlNaming.cs
+++ FileNaming/UrlNaming.cs
@@ -1,4 +1,8 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace FileNaming
 {
@@ -20,14 +24,68 @@ namespace FileNaming
 
         public static string BuildUrlSafePath(string basePath)
         {
-            string root = basePath.Trim() + "/";
-
+            string root = RemoveDiacritics(basePath.Trim() + "/", true, NormaliseLWithStroke);
+            root = RemoveApostrophes(root);
 
             return NoHyphensNextToSlash.Replace(
                 NoRepeatingHyphens.Replace(
                     AcceptableUrlCharacters.Replace(root.Replace(@"\", @"/"), Replacementchar),
                     Replacementchar),
                 "/").TrimEnd(Replacementchar.ToCharArray()).ToLowerInvariant();
+        }
+
+        private static string RemoveApostrophes(string root)
+        {
+            return root.Replace("'s", "s").Replace("'S", "S");
+        }
+
+
+        internal static IEnumerable<char> RemoveDiacriticsEnum(string src, bool compatNorm,
+                                                               Func<char, char> customFolding)
+        {
+            foreach (char c in src.Normalize(compatNorm ? NormalizationForm.FormKD : NormalizationForm.FormD))
+                switch (CharUnicodeInfo.GetUnicodeCategory(c))
+                {
+                    case UnicodeCategory.NonSpacingMark:
+                    case UnicodeCategory.SpacingCombiningMark:
+                    case UnicodeCategory.EnclosingMark:
+                        //do nothing
+                        break;
+                    default:
+                        yield return customFolding(c);
+                        break;
+                }
+        }
+
+        internal static IEnumerable<char> RemoveDiacriticsEnum(string src, bool compatNorm)
+        {
+            return RemoveDiacritics(src, compatNorm, c => c);
+        }
+
+        public static string RemoveDiacritics(string src, bool compatNorm, Func<char, char> customFolding)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in RemoveDiacriticsEnum(src, compatNorm, customFolding))
+                sb.Append(c);
+            return sb.ToString();
+        }
+
+        public static string RemoveDiacritics(string src, bool compatNorm)
+        {
+            return RemoveDiacritics(src, compatNorm, c => c);
+        }
+
+        private static char NormaliseLWithStroke(char c)
+        {
+            switch (c)
+            {
+                case 'ł':
+                    return 'l';
+                case 'Ł':
+                    return 'L';
+                default:
+                    return c;
+            }
         }
     }
 }
